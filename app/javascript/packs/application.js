a5af10eccc9edd3ad5c9ba853c56b1ce3451be8d
Caio Andrade <andrade@hey.com>
2020-11-18 18:05:59 -0600

    Lecture end

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,19 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
+// Rails dependencies
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// External libraries
+import 'bootstrap';
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// Import components
+import { initUpdateNavbarOnScroll } from "../components/navbar";
+import { loadDynamicBannerText } from '../components/banner';
+
+// Call initializers
+document.addEventListener('turbolinks:load', () => {
+  initUpdateNavbarOnScroll();
+  loadDynamicBannerText();
+});
+
